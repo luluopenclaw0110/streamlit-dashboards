d44c7178082d1def282f9d1eabbf920689f3f20a
luluopenclaw0110 <luluopenclaw0110@gmail.com>
2026-03-20 02:03:05 +0800

    Auto column width

--- miyako_dashboard.py
+++ miyako_dashboard.py
@@ -142,7 +142,9 @@ if selected_dest == "📊 超級比一比":
             })
         
         comp_df = pd.DataFrame(comparison_data)
-        st.dataframe(comp_df, hide_index=True, use_container_width=True, height=400)
+        # 自動調整欄寬
+        styler = comp_df.style.set_properties(**{'white-space': 'normal', 'text-align': 'left'})
+        st.dataframe(styler, hide_index=True, use_container_width=True)
         
         st.info("💡 去程7/19，回程7/25，機票2大2小 / 飯店每晚")
     else:
